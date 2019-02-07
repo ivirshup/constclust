68675577fed001239e5a43e3099647116c98ab06
Isaac Virshup <ivirshup@gmail.com>
2019-02-07 19:11:21 +1100

    Formatting

--- constclust/aggregate.py
+++ constclust/aggregate.py
@@ -18,25 +18,27 @@ ClusterRef = namedtuple("ClusterRef", ("clustering_id", "cluster_id"))
 # I wouldn't get those index names, but I can get that later.
 class ClusterIndexer(object):
     """
-    API convenience which allows indexing access to parent Reconciler's 
+    API convenience which allows indexing access to parent Reconciler's
     clusters.
     """
+
     def __init__(self, parent):
         self._parent = parent
-    
+
     def __len__(self):
         return len(self._parent._mapping)
-    
+
     # Note: I might want to modify this one in the future, for specialized access
     def __getitem__(self, key):
         return self._parent._mapping[key]
-    
+
     def __iter__(self):
         return self._parent._mapping.__iter__()
-    
+
     def __contains__(self, key):
         # This should be able to check for a clustering, or a cluster
         return self._parent._mapping.__contains__(key)
+
 
 class Component(object):
     """
@@ -45,9 +47,9 @@ class Component(object):
     Attributes
     ----------
     _parent : Reconciler
-        The reconciler which generated this component
+        The Reconciler which generated this component.
     settings : pd.DataFrame
-        Subset of parents settings. Contains only settings for clustering 
+        Subset of parents settings. Contains only settings for clustering
         which appear in this component.
     _mapping : pd.Series
         Partial view of parents `_mapping`.
@@ -56,14 +58,16 @@ class Component(object):
     union : np.array
         Union of clusters in this component.
     """
+
     def __init__(self, reconciler, cluster_ids):
         self._parent = reconciler
         self._mapping = self._parent._mapping.loc[cluster_ids]
-        clusterings = self._mapping.index.get_level_values(
-            "clustering").unique()
+        clusterings = self._mapping.index.get_level_values("clustering").unique()
         self.settings = self._parent.settings.loc[clusterings]
         # self.clusters = ClusterIndexer(self)
-        self.intersect = reduce(partial(np.intersect1d, assume_unique=True), self._mapping.values)
+        self.intersect = reduce(
+            partial(np.intersect1d, assume_unique=True), self._mapping.values
+        )
         self.union = reduce(np.union1d, self._mapping.values)
 
     def one_hot(self, selection="intersect"):
@@ -73,13 +77,15 @@ class Component(object):
         elif selection == "union":
             encoding[self.union] = True
         else:
-            raise ValueError(f"Parameter `selection` must be either 'intersect' or 'union', was '{selection}'")
-    
+            raise ValueError(
+                f"Parameter `selection` must be either 'intersect' or 'union', was '{selection}'"
+            )
+
     def __len__(self):
         return len(self._mapping)
-    
+
     def __repr__(self):
-        return f"<Component n_solutions={len(self)}, max_cells={len(self.union)}, min_cells={len(self.intersect)}>" 
+        return f"<Component n_solutions={len(self)}, max_cells={len(self.union)}, min_cells={len(self.intersect)}>"
 
 
 class Reconciler(object):
@@ -89,15 +95,15 @@ class Reconciler(object):
     Attributes
     ----------
     settings : pd.DataFrame
-        Contains settings for all clusterings. Index corresponds to 
-        `.clusterings` columns, while columns should correspond to the 
+        Contains settings for all clusterings. Index corresponds to
+        `.clusterings` columns, while columns should correspond to the
         parameters which were varied.
     clusterings : pd.DataFrame
-        Contains cluster assignments for each cell, for each clustering. 
-        Columns correspond to `.settings` index, while the index correspond 
+        Contains cluster assignments for each cell, for each clustering.
+        Columns correspond to `.settings` index, while the index correspond
         to the cells.
     _obs_names : pd.Index
-        Ordered set for names of the cells. Internally they are refered to by 
+        Ordered set for names of the cells. Internally they are refered to by
         integer positions.
     _mapping : pd.Series
         Series which maps clustering and cluster id to that contents clusters.
@@ -106,6 +112,7 @@ class Reconciler(object):
         and cluster ids). Edges connect clusters with shared contents. Weight
         is the Jaccard similarity between the contents of the clusters.
     """
+
     def __init__(self, settings, clusterings):
         assert all(settings.index == clusterings.columns)
         self.settings = settings
@@ -115,19 +122,21 @@ class Reconciler(object):
         self.clusters = ClusterIndexer(self)
         self.graph = nx.Graph()
         self.graph.add_weighted_edges_from(build_graph(settings, clusterings))
-    
+
     def get_components(self, min_weight):
         """
         Return connected components of graph, with edges filtered by min_weight
         """
-        sub_g = self.graph.edge_subgraph(filter(
-            lambda x: self.graph.edges[x]["weight"] > min_weight, 
-            self.graph.edges))
+        sub_g = self.graph.edge_subgraph(
+            filter(
+                lambda x: self.graph.edges[x]["weight"] > min_weight, self.graph.edges
+            )
+        )
         comps = []
         for graph_comp in nx.connected_components(sub_g):
             comps.append(Component(self, list(graph_comp)))
         return comps
-    
+
     def get_param_range(self, clusters):
         """
         Given a set of clusters, returns the range of parameters for which they were calculated.
@@ -156,14 +165,19 @@ def build_graph(settings, clusters):
     for clustering1, clustering2 in neighbors:
         clusters1 = mapping[clustering1]
         clusters2 = mapping[clustering2]
-        for (id1, clust1), (id2, clust2) in product(clusters1.items(), clusters2.items()):
-            intersect = np.intersect1d(clust1, clust2, 
-                                       assume_unique=True)
+        for (id1, clust1), (id2, clust2) in product(
+            clusters1.items(), clusters2.items()
+        ):
+            intersect = np.intersect1d(clust1, clust2, assume_unique=True)
             if len(intersect) > 0:
-                jaccard_sim = intersect.size / \
-                    (clust1.size + clust2.size - intersect.size)
-                edge = (ClusterRef(clustering1, id1),
-                        ClusterRef(clustering2, id2), jaccard_sim)
+                jaccard_sim = intersect.size / (
+                    clust1.size + clust2.size - intersect.size
+                )
+                edge = (
+                    ClusterRef(clustering1, id1),
+                    ClusterRef(clustering2, id2),
+                    jaccard_sim,
+                )
                 graph.append(edge)
     return graph
 
@@ -191,8 +205,8 @@ def gen_mapping(clusterings):
     Returns
     -------
     pd.Series
-        Mapping to cluster contents. Index is a MultiIndex with levels 
-        "clustering", "cluster". Values are np.arrays of 
+        Mapping to cluster contents. Index is a MultiIndex with levels
+        "clustering", "cluster". Values are np.arrays of cluster contents.
     """
     keys = []
     values = []
@@ -200,10 +214,10 @@ def gen_mapping(clusterings):
         for cluster_id in clustering.unique():
             keys.append((clustering_id, cluster_id))
             values.append(np.where(clustering == cluster_id)[0])
-    mapping = pd.Series(values,
-                        index=pd.MultiIndex.from_tuples(keys, names=["clustering", "cluster"]))
+    mapping = pd.Series(
+        values, index=pd.MultiIndex.from_tuples(keys, names=["clustering", "cluster"])
+    )
     return mapping
-
 
 
 # @numba.njit
@@ -227,11 +241,10 @@ def gen_mapping(clusterings):
 #     return graph
 
 
-
 # def gen_mapping(settings, clusters):
 #     """
 #     Create a mapping from parameters to clusters to contents
-    
+
 #     Args:
 #         settings (pd.DataFrame)
 #         clusters (pd.DataFrame)
@@ -244,6 +257,4 @@ def gen_mapping(clusterings):
 #         for cluster_id in cluster.unique():
 #             solution[cluster_id] = np.where(cluster == cluster_id)[0]
 #     return mapping
-
-
 
