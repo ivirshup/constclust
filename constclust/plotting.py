717a84ad168ded7b9862fe14ecbcf74f00841a3b
Isaac Virshup <ivirshup@gmail.com>
2020-12-01 19:58:37 +1100

    fix tick labelling for heatmaps

--- constclust/plotting.py
+++ constclust/plotting.py
@@ -16,24 +16,13 @@ from typing import Optional
 # TODO: Think up visualization for the set of components
 
 
-def _fix_seaborn_float_labels(axis):
-    """
-    Seaborn is bad at formatting floating point on axes, and fixing it there
-    would probably involve learning about unicode in python2. This takes an
-    axis (like, x-axis, not plt.Axes) and formats the floats better.
-    """
-    fmt = axis.get_major_formatter()
-    fmt.seq = ["{:g}".format(float(s)) for s in fmt.seq]
-    axis.set_major_formatter(fmt)
-
-
 # TODO: Add title arg
 # TODO: crosstab instead of pivot_table?
 def component_param_range(
     component: "aggregate.Component",
     x: str = "n_neighbors",
     y: str = "resolution",
-    ax: Optional[mpl.axis.Axis] = None
+    ax: Optional[mpl.axis.Axis] = None,
 ) -> mpl.axis.Axis:
     """
     Given a component, show which parameters it's found at as a heatmap.
@@ -59,8 +48,10 @@ def component_param_range(
     ncolors = param_states.value_counts().max() + 1
     cmap = mpl.colors.LinearSegmentedColormap.from_list(
         "dummy_name",
-        sns.cm.rocket(np.linspace(20, 240, ncolors, dtype=int)),  # Shifted a little so it's prettier
-        ncolors
+        sns.cm.rocket(
+            np.linspace(20, 240, ncolors, dtype=int)
+        ),  # Shifted a little so it's prettier
+        ncolors,
     )
 
     # Initialize blank array
@@ -72,6 +63,12 @@ def component_param_range(
     )
     data = (data + params).fillna(0).astype(int)
 
+    # Fix axis labels
+    if is_float_dtype(data.index.dtype):
+        data.index = data.index.map(lambda x: "{:g}".format(x))
+    if is_float_dtype(data.columns.dtype):
+        data.columns = data.columns.map(lambda x: "{:g}".format(x))
+
     ax = sns.heatmap(
         data,
         linewidths=0.2,
@@ -80,12 +77,6 @@ def component_param_range(
         vmax=ncolors,
         cmap=cmap,
     )
-
-    # Fix axis labels
-    if is_float_dtype(component._parent.settings[x].dtype):
-        _fix_seaborn_float_labels(ax.xaxis)
-    if is_float_dtype(component._parent.settings[y].dtype):
-        _fix_seaborn_float_labels(ax.yaxis)
 
     # Discretize colorbar
     cb = ax.collections[0].colorbar
@@ -117,7 +108,7 @@ def umap(component, adata, ax=None, umap_kwargs={}):
 
     cell_names = component._parent._obs_names
     # if not all(cell_names.isin(adata.obs_names)):
-        # raise ValueError("Counldn't find all cells in component's parent in adata.")
+    # raise ValueError("Counldn't find all cells in component's parent in adata.")
     cell_value = pd.Series(0, index=adata.obs_names, dtype=float)
     # present_freqs = component.cell_frequency
     # cell_value[present_freqs.index] = present_freqs
@@ -126,15 +117,17 @@ def umap(component, adata, ax=None, umap_kwargs={}):
         cell_value[component._parent._mapping.iloc[cluster]] += 1
     cell_value = cell_value / cell_value.max()
     adata.obs["_tmp"] = cell_value
-    if (len(cell_names) < len(adata.obs_names)):
+    if len(cell_names) < len(adata.obs_names):
         # Take view
         sc.pl.umap(adata[cell_names, :], color="_tmp", ax=ax, **umap_kwargs)
-    else: 
+    else:
         sc.pl.umap(adata[cell_names, :], color="_tmp", ax=ax, **umap_kwargs)
     adata.obs.drop(columns="_tmp", inplace=True)
 
 
-def global_stability(settings, clusters, x="n_neighbors", y="resolution", cmap=sns.cm.rocket, ax=None):
+def global_stability(
+    settings, clusters, x="n_neighbors", y="resolution", cmap=sns.cm.rocket, ax=None
+):
     # This should probably aggregate, currently do hacky thing of just subsetting
     if len(set(settings[[x, y]].itertuples(index=False))) != len(settings):
         # raise NotImplementedError("Aggregation of multiple global solutions not yet implemented.")
@@ -144,10 +137,12 @@ def global_stability(settings, clusters, x="n_neighbors", y="resolution", cmap=s
     ylabs = sorted(settings[y].unique())
 
     clusters = clusters[settings.index].copy()
-    mapping = dict(zip(settings.index, settings[[x, y]].itertuples(index=False, name=None)))
+    mapping = dict(
+        zip(settings.index, settings[[x, y]].itertuples(index=False, name=None))
+    )
     # + .5 might make it align better with local plot
-    xpos = np.arange(len(xlabs))# + .5
-    ypos = np.arange(len(ylabs))# + .5
+    xpos = np.arange(len(xlabs))  # + .5
+    ypos = np.arange(len(ylabs))  # + .5
     pos_map = {}
     for (xlab, x), (ylab, y) in product(zip(xlabs, xpos), zip(ylabs, ypos)):
         pos_map[(xlab, ylab)] = (x, y)
@@ -181,13 +176,14 @@ def global_stability(settings, clusters, x="n_neighbors", y="resolution", cmap=s
     cb.outline.set_visible(False)
 
 
-def component(component: "Component",
-              adata: AnnData,
-              x: str = "n_neighbors",
-              y: str = "resolution",
-              plot_global: bool = False,
-              aspect: float = None,
-              umap_kwargs: dict = {}
+def component(
+    component: "Component",
+    adata: AnnData,
+    x: str = "n_neighbors",
+    y: str = "resolution",
+    plot_global: bool = False,
+    aspect: float = None,
+    umap_kwargs: dict = {},
 ):
     """
     Plot stability and UMAP for component.
@@ -209,15 +205,17 @@ def component(component: "Component",
     """
     if aspect is None:
         if plot_global:
-            aspect = 1/3
+            aspect = 1 / 3
         else:
-            aspect = 1/2
+            aspect = 1 / 2
     fig = plt.figure(figsize=mpl.figure.figaspect(aspect))
     if plot_global:
         reconciler = component._parent
         gs = fig.add_gridspec(1, 3)
         global_ax = fig.add_subplot(gs[0, 1])
-        global_stability(reconciler.settings, reconciler.clusterings, x, y, ax=global_ax)
+        global_stability(
+            reconciler.settings, reconciler.clusterings, x, y, ax=global_ax
+        )
     else:
         gs = fig.add_gridspec(1, 2)
     heatmap_ax = fig.add_subplot(gs[0, 0])
@@ -230,13 +228,14 @@ def component(component: "Component",
 def edge_weight_distribution(recon, **kwargs):
     return sns.histplot(recon.graph.es["weight"], **kwargs)
 
+
 # Modified from seaborn
 def _tick_step(ax, labels, axis):
     transform = ax.figure.dpi_scale_trans.inverted()
     bbox = ax.get_window_extent().transformed(transform)
     size = [bbox.width, bbox.height][axis]
     axis = [ax.xaxis, ax.yaxis][axis]
-    tick, = axis.set_ticks([0])
+    (tick,) = axis.set_ticks([0])
     fontsize = tick.label.get_size()
     max_ticks = int(size // (fontsize / 72))
     tick_every = len(labels) // max_ticks + 1
