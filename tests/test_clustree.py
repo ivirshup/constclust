3db3e3a0d5b53a4b2c97eaad5262b6d471dafef5
Isaac Virshup <ivirshup@gmail.com>
2021-02-11 18:49:01 +1100

    Add weights to test

--- tests/test_clustree.py
+++ tests/test_clustree.py
@@ -18,7 +18,13 @@ import pytest
                     "c": [0, 0, 0, 1, 1, 2, 2, 2, 2, 2],
                 }
             ),
-            nx.DiGraph({0: [1, 2], 1: [3, 4], 2: [6]}),
+            nx.DiGraph(
+                {
+                    0: {1: {"weight": 0.5}, 2: {"weight": 0.5}},
+                    1: {3: {"weight": 0.6}, 4: {"weight": 0.4}},
+                    2: {6: {"weight": 1.0}},
+                }
+            ),
         ),
         (
             pd.DataFrame(
@@ -28,12 +34,22 @@ import pytest
                     "c": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 }
             ),
-            nx.DiGraph({0: [1, 2], 1: [3], 2: [3]}),
+            nx.DiGraph(
+                {
+                    0: {1: {"weight": 0.5}, 2: {"weight": 0.5}},
+                    1: {3: {"weight": 0.5}},
+                    2: {3: {"weight": 0.5}},
+                }
+            ),
         ),
     ],
     ids=["tree", "diamond"],
 )
-def test_clustree_generation_isomorphic(clusterings, expected):
+def test_clustree_weights(clusterings, expected):
+    import networkx.algorithms.isomorphism as iso
+
     result = gen_clustree(clusterings)
 
-    assert nx.is_isomorphic(expected, result)
+    assert nx.is_isomorphic(
+        expected, result, edge_match=iso.numerical_edge_match("weight", default=1)
+    )
